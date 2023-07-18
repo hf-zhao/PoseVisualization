286cec9092bbb35d44c5a642de4254515fc61dbc
zhf <hf_zhao@outlook.com>
2023-07-18 23:42:21 +0800

    补交四元数转旋转矩阵

--- quat2mat.m
+++ quat2mat.m
@@ -1,12 +1,17 @@
-function transform = quat2mat(quaternion)
+function transform = quat2mat(quaternion, varargin)
 %quat2mat - Description
 %
 % Syntax: transform = quat2mat(quaternion)
 %
 % Long description
+    opt = argsParse(varargin);
+    
     quaternion = quaternion/norm(quaternion);
     s = quaternion(1);
     v = quaternion(2:4);
     v_mat = [0 -v(3) v(2); v(3) 0 -v(1); -v(2) v(1) 0];
     transform = v'*v + s^2*eye(3) + 2*s*v_mat + v_mat^2;
+    if opt.T
+        rotationMatrix = [rotationMatrix, zeros(3,1); zeros(1,3),1];
+    end
 end
